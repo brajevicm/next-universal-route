610058282aa472c40afc79022d325d80cc689c3e
Miloš Brajević <brajevicms@gmail.com>
2019-10-30 00:12:54 +0100

    Some Router arguments can be optional.

--- src/Router.ts
+++ src/Router.ts
@@ -1,11 +1,14 @@
 import NextRouter, { NextRouter as NextRouterType } from 'next/router';
+
 import { NextRoute } from './NextRoute';
 
 export const Router = ((router: NextRouterType) => {
-  const push = (href: NextRoute, options: object) =>
+  const push = (href: NextRoute, options?: object) =>
     router.push(href.toHref(), href.toAs(), options);
-  const replace = (href: NextRoute, options: object) =>
+
+  const replace = (href: NextRoute, options?: object) =>
     router.replace(href.toHref(), href.toAs(), options);
+
   const prefetch = (href: NextRoute) => router.prefetch(href.toHref());
 
   return {
