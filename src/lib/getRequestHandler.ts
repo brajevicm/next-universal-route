ce8c189c74790dbdaf3022dc5f8200fcfe76b28b
Milos Brajevic <milos.brajevic@kupujemprodajem.com>
2019-12-04 12:27:11 +0100

    Added options for subdomain.

--- src/lib/getRequestHandler.ts
+++ src/lib/getRequestHandler.ts
@@ -1,6 +1,6 @@
 import { Routes } from './Routes';
 
-export const getRequestHandler = (app, routes) => {
+export const getRequestHandler = (app, routes, options) => {
   const nextHandler = app.getRequestHandler();
   const router = new Routes(routes);
 
@@ -8,6 +8,10 @@ export const getRequestHandler = (app, routes) => {
     const route = router.getRoute(req.url);
 
     if (route) {
+      if (req.subdomains.indexOf(options.subdomain) > -1) {
+        app.render(req, res, `/${options.subdomain}${route.page}`, route.query);
+      }
+
       app.render(req, res, route.page, route.query);
     } else {
       nextHandler(req, res, req.url);
