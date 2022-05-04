e20e36f0f2db2cec321efce76d75fd4f249b9131
brajevicm <brajevicms@gmail.com>
2022-05-04 23:35:51 +0200

    Fixed the Router when passing absolute urls.

--- src/lib/Router.ts
+++ src/lib/Router.ts
@@ -2,29 +2,13 @@ import NextRouter, { NextRouter as NextRouterType } from 'next/router';
 
 import { Route } from './Route';
 import { NextRoute } from './NextRoute';
-
-// import { clone } from './lib/deepClone';
-
-// TODO: Find the way to replace Next's Router completely
-// const ClonedRouter = ((router: NextRouterType) => {
-//   const newRouter = clone(router);
-
-//   newRouter.push = (href: NextRoute, options?: object) =>
-//     router.push(href.toHref()(), href.toAs()(), options);
-
-//   newRouter.prefetch = (href: NextRoute) => router.prefetch(href.toHref()());
-
-//   newRouter.replace = (href: NextRoute, options?: object) =>
-//     router.replace(href.toHref()(), href.toAs()(), options);
-
-//   return newRouter;
-// })(NextRouter);
+import { isAbsolutePath } from '../utils/isAbsolutePath';
 
 export const Router = ((router: NextRouterType) => {
   const push = (href: NextRoute, options?: object) => {
     const newHref = typeof href === 'string' ? href : href.toHref();
 
-    if (typeof href === 'string' || href.isAbsolutePath) {
+    if (typeof href === 'string' || isAbsolutePath(href.path)) {
       return (window.location.href = newHref);
     }
 
@@ -48,6 +32,6 @@ export const Router = ((router: NextRouterType) => {
     push,
     prefetch,
     replace,
-    update
+    update,
   };
 })(NextRouter);
